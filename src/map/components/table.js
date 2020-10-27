343f9fea1b894f163055cbbc9896ec1b503c21eb
KM8Oz <ouazmir@gmail.com>
2020-10-27 14:01:21 +0400

    v1.0.2

--- src/map/components/table.js
+++ src/map/components/table.js
@@ -1,117 +1,62 @@
 import * as React from 'react';
-import { XGrid } from '@material-ui/x-grid';
-import { GridOverlay, DataGrid } from '@material-ui/data-grid';
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
 import { Rnd } from 'react-rnd';
-import { makeStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
-const columns = [
- { field: 'id', headerName: 'id',type: 'number',width:56},
-  { field: 'fishery_id', headerName: 'fishery_id'},
-  {
-    field: 'exploiting_stocks',
-    headerName: 'exploiting stocks',
-    type: 'number',
-    // width: 100,
-  },
-  {
-    field: 'fishery_name',
-    headerName: 'fishery Name',
-   // description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-     width: 200,
-    // valueGetter: (params) =>
-    //   `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
-];
-function CustomLoadingOverlay() {
-            return (
-                <GridOverlay >
-                <div style={{ position: 'absolute', top: 0, width: '100%' }}>
-                  <LinearProgress />
-                </div>
-              </GridOverlay>
-            )
+// import 'rsuite-table/dist/css/rsuite-table.css'
 
-  }
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      border: 0,
-      color:
-        theme.palette.type === 'light'
-          ? 'rgba(0,0,0,.85)'
-          : 'rgba(255,255,255,0.85)',
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      WebkitFontSmoothing: 'auto',
-      letterSpacing: 'normal',
-      '& .MuiDataGrid-columnsContainer': {
-        backgroundColor: theme.palette.type === 'light' ? '#fafafa' : '#1d1d1d',
-      },
-      '& .MuiDataGrid-iconSeparator': {
-        display: 'none',
-      },
-      '& .MuiDataGrid-colCell, .MuiDataGrid-cell': {
-        maxWidth:`fit-content !important`,
-        borderRight: `1px solid ${
-          theme.palette.type === 'light' ? '#f0f0f0' : '#303030'
-        }`,
-      },
-      '& .MuiDataGrid-columnsContainer, .MuiDataGrid-cell': {
-        maxWidth:`fit-content !important`,
-        borderBottom: `1px solid ${
-          theme.palette.type === 'light' ? '#f0f0f0' : '#303030'
-        }`,
-      },
-      '& .MuiDataGrid-cell': {
-        maxWidth:`fit-content !important`,
-        color:
-          theme.palette.type === 'light'
-            ? 'rgba(0,0,0,.85)'
-            : 'rgba(255,255,255,0.65)',
-      },
-      '& .MuiPaginationItem-root': {
-        borderRadius: 0,
-      },
-      '&.MuiDataGrid-root .MuiDataGrid-cell':{
-        maxWidth:`fit-content !important`,
-      }
-    },
-  }));
-  
-export default function DataTable({rows}) {
-    const classes = useStyles();
-  return (
-    <Rnd 
+
+
+const ImageCell = ({ rowData, dataKey, ...rest }) => (
+  <Cell {...rest}>
+    <img src={rowData[dataKey]} width="50" />
+  </Cell>
+);
+
+const DataTable = ({rows}) => {
+  console.log(rows);
+  return(<>
+   <Rnd 
     default={{
         x: 0,
         y: 0,
         width: 400,
-        height: 400,
+        height:300
        }}
          >
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid 
-    //    components={{
-    //       loadingOverlay: CustomLoadingOverlay(state),
-    //     }}
-        //  loading
-         //columns={columns}
-         onCellClick={(evt)=>{
-             console.log('ok',evt);
-         }}
-         pagination
-        {...{columns:columns,rows:rows}}/>
-    </div>
-</Rnd>
-  );
-}
+  <Table data={rows} wordWrap
+   style={{fontSize:'.6em'}}
+   height={300} 
+  >
+    <Column width={100}  resizable align={'center'} 
+         style={{padding:'unset',margin:'unset',width:'200px',maxWidth:'100%'  ,display: 'flex',
+         alignItems: 'center',
+         justifyContent: 'center'}} >
+      <HeaderCell>код(fao.org)</HeaderCell>
+      <Cell dataKey="fishery_id"  />
+    </Column>
+
+    <Column width={200} align={'center'} 
+     style={{padding:'unset',margin:'unset',width:'200px',maxWidth:'100%'  ,display: 'flex',
+     alignItems: 'center',
+     justifyContent: 'center'}}
+    resizable>
+      <HeaderCell>промысла</HeaderCell>
+      <Cell dataKey="fishery_name"   />
+    </Column>
+
+    <Column width={100} align={'center'}   resizable
+         style={{padding:'unset',margin:'unset',width:'200px',maxWidth:'100%'  ,display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'}}>
+      <HeaderCell>запас</HeaderCell>
+      <Cell dataKey='exploiting_stocks' />
+    </Column>
+
+    {/* <Column width={100} resizable>
+      <HeaderCell>map_info</HeaderCell>
+      <ImageCell dataKey="map_info" />
+    </Column> */}
+  </Table>
+  </Rnd>
+  </>)
+};
+export default DataTable;
